d77f7ba1dab2b6da854efd7a9ea32863c108faf4
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-10-28 18:48:06 -0300

    set variable to don't enable packages on startup before init

---
+++ early-init.el
@@ -0,0 +1,17 @@
+;;; early-init.el --- <-> -*- lexical-binding: t; -*-
+;;
+;; Author: Guilherme Ananias <https://github.com/noghartt>
+;; Maintainer: Guilherme Ananias <hi@guiananias.dev>
+;; Created: October 28, 2021
+;; Modified: October 28, 2021
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+;; Set this variable to `nil' to prevent that `package.el' load packages prior
+;; See details here: https://github.com/raxod502/straight.el#getting-started
+(setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;;; early-init.el ends here
